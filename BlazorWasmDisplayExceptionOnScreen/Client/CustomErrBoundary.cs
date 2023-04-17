06cef0c4e777f1abb14074e819aaab2efb365c10
[Thammapat.C] <[thammapat@gmail.com]>
2023-04-17 19:11:03 +0700

    Add project files.

---
+++ BlazorWasmDisplayExceptionOnScreen/Client/CustomErrBoundary.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorWasmDisplayExceptionOnScreen.Client
+{
+    public class CustomErrBoundary : ErrorBoundary
+    {
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            //
+            // write exception to database
+            //
+            return base.OnErrorAsync(exception);
+        }
+    }
+}
